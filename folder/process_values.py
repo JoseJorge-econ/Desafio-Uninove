b87446e6964e3ef58c56db6b8d48abd8a2dda60e
JoseJorge-econ <67654394+JoseJorge-econ@users.noreply.github.com>
2021-08-19 09:51:50 -0300

    Delete folder directory

--- folder/process_values.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data.csv')
-
-print(df.to_string()) 
-print('\n')
-print('min value:', df['price'].min()) 
-print('max value:', df['price'].max()) 
-print('mean value:', df['price'].mean()) 
